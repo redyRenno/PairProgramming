d496b5b2be08d5deec02b773f11a101e4c616b45
redyRenno <redyrenno@gmail.com>
2022-04-21 13:55:39 +0900

    migration

---
+++ U-mad/migrations/20220421033042-create-user-detail.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('UserDetails', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      birthDate: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      phoneNumber: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('UserDetails');
+  }
+};
---
+++ U-mad/migrations/20220421034304-create-course.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Courses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      videoURL: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Courses');
+  }
+};
